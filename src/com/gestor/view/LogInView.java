df36f660358fbb163eba228ab706a93e2e405f3b
JosemaDev06 <josemaestudiosdam@gmail.com>
2026-05-14 13:15:29 +0200

    upgrade login view v1.1 and create SignUp view

--- src/com/gestor/view/LogInView.java
+++ src/com/gestor/view/LogInView.java
@@ -5,27 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class LogInView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LogInView frame = new LogInView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Create the frame.
@@ -36,7 +30,43 @@ public class LogInView extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel ButtonsPanel = new JPanel();
+		contentPane.add(ButtonsPanel, BorderLayout.SOUTH);
+		
+		JButton btnLogIn = new JButton("Log In");
+		btnLogIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		ButtonsPanel.add(btnLogIn);
+		
+		JButton btnSignUp = new JButton("Sign Up");
+		ButtonsPanel.add(btnSignUp);
+		
+		JPanel TextFieldsPanel = new JPanel();
+		contentPane.add(TextFieldsPanel, BorderLayout.CENTER);
+		TextFieldsPanel.setLayout(null);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEmail.setBounds(65, 71, 54, 13);
+		TextFieldsPanel.add(lblEmail);
+		
+		textField = new JTextField();
+		textField.setBounds(154, 70, 186, 18);
+		TextFieldsPanel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPassword.setBounds(65, 128, 79, 13);
+		TextFieldsPanel.add(lblPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(154, 127, 186, 18);
+		TextFieldsPanel.add(passwordField);
 
 	}
-
 }
