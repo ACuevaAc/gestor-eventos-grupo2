f015993cb55769c64747fcf3bc9cb9ea0c135023
ACuevaAc <acuevasacosta@gmail.com>
2026-05-18 12:27:02 +0200

    feat: create table controller

--- src/com/gestor/controller/gestorMesasController.java
+++ src/com/gestor/controller/gestorMesasController.java
@@ -1,5 +1,10 @@
 package com.gestor.controller;
 
+import java.awt.Color;
+import java.util.List;
+
+import javax.swing.JButton;
+
 import com.gestor.model.entity.Mesa;
 import com.gestor.service.mesaService;
 import com.gestor.view.admin.AdminMainView;
@@ -8,12 +13,15 @@ import com.gestor.view.admin.GestorMesasView;
 public class gestorMesasController {
 	private GestorMesasView view;
 	private mesaService ms;
+	private AdminController aCont;
 	
-	public gestorMesasController(GestorMesasView v) {
+	public gestorMesasController(GestorMesasView v,AdminController cont) {
 		
 		this.view=v;
+		this.aCont=cont;
 		
 		ms= new mesaService();
+		cargarMesas();
 		v.getBtnRegistrarModificar().addActionListener(e-> registrarMesa());
 		v.getBtnAtras().addActionListener(e-> volverMenu());
 		
@@ -34,7 +42,21 @@ public class gestorMesasController {
 		mesa.setMesa_Reservada(false);
 		ms.crearMesa(mesa);
 		
-		int id=mesa.getId();
+		cargarMesas();
 	}
+	public void cargarMesas() {
+	    List<JButton> lista = aCont.getAdminView().getMesasList();
+	    int idsActuales = ms.obtenerIdsMesas(); 
+	 
+	    int limite = Math.min(idsActuales, lista.size());
+	    
+	    for (int i = 0; i < limite; i++) {
+	        lista.get(i).setBackground(Color.GREEN);
+	    }
+	    if (idsActuales > lista.size()) {
+	        System.out.println("LOG GestionMesasController: Hay " + idsActuales + " mesas en la BD, pero solo " + lista.size() + " botones en la pantalla.");
+	    }
+	}
+
 
 }
