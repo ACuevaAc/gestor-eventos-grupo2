b61cfb0ad74a4ce750652d11fa8c6549c6187e81
JosemaDev06 <josemaestudiosdam@gmail.com>
2026-05-18 10:23:35 +0200

    add: gestormesaview v1.1

--- src/com/gestor/view/admin/GestorMesasView.java
+++ src/com/gestor/view/admin/GestorMesasView.java
@@ -21,23 +21,9 @@ public class GestorMesasView extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtID;
 	private JTextField txtNombre;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GestorMesasView frame = new GestorMesasView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private JButton btnRegistrarModificar;
+	private JButton btnAtras;
+	private JComboBox cBgente;
 	/**
 	 * Create the frame.
 	 */
@@ -48,46 +34,74 @@ public class GestorMesasView extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panelBotones = new JPanel();
 		contentPane.add(panelBotones, BorderLayout.SOUTH);
-		
-		JButton btnRegistrarModificar = new JButton("Register/Modify");
+
+		btnRegistrarModificar = new JButton("Register/Modify");
 		panelBotones.add(btnRegistrarModificar);
-		
-		JButton btnAtras = new JButton("Back");
+
+		btnAtras = new JButton("Back");
 		panelBotones.add(btnAtras);
-		
-		
+
 		JPanel panelMesa = new JPanel();
 		contentPane.add(panelMesa, BorderLayout.CENTER);
 		panelMesa.setLayout(new GridLayout(4, 2, 0, 0));
-		
+
 		JLabel lblId = new JLabel("Id");
 		panelMesa.add(lblId);
-		
+
 		txtID = new JTextField();
 		txtID.setEditable(false);
 		panelMesa.add(txtID);
 		txtID.setColumns(10);
-		
+
 		JLabel lblNombre = new JLabel("Nombre");
 		panelMesa.add(lblNombre);
-		
+
 		txtNombre = new JTextField();
 		panelMesa.add(txtNombre);
 		txtNombre.setColumns(10);
-		
+
 		JLabel lblGente = new JLabel("Máximo de comensales");
 		panelMesa.add(lblGente);
-		
-			
-		JComboBox cBgente = new JComboBox<>();
-		for(int i = 1 ; i <=10 ; i ++) {
+
+		cBgente = new JComboBox<>();
+		for (int i = 1; i <= 10; i++) {
 			cBgente.addItem(i);
 		}
 		panelMesa.add(cBgente);
 
+	}
+	public JTextField getTxtID() {
+		return txtID;
+	}
+	public void setTxtID(JTextField txtID) {
+		this.txtID = txtID;
+	}
+	public JTextField getTxtNombre() {
+		return txtNombre;
+	}
+	public void setTxtNombre(JTextField txtNombre) {
+		this.txtNombre = txtNombre;
+	}
+	public JButton getBtnRegistrarModificar() {
+		return btnRegistrarModificar;
+	}
+	public void setBtnRegistrarModificar(JButton btnRegistrarModificar) {
+		this.btnRegistrarModificar = btnRegistrarModificar;
+	}
+	public JButton getBtnAtras() {
+		return btnAtras;
+	}
+	public void setBtnAtras(JButton btnAtras) {
+		this.btnAtras = btnAtras;
+	}
+	public JComboBox getcBgente() {
+		return cBgente;
+	}
+	public void setcBgente(JComboBox cBgente) {
+		this.cBgente = cBgente;
 	}
 
 }
