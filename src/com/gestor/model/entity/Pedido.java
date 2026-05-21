fd31a3f708eb349228048068fabc5ddba5114922
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-05-21 12:50:39 +0200

    modify: Pedido class is now according to 'Pedir' table

--- src/com/gestor/model/entity/Pedido.java
+++ src/com/gestor/model/entity/Pedido.java
@@ -1,28 +1,111 @@
 package com.gestor.model.entity;
 
+/**
+ * @class Pedido
+ * @description Entity class representing a record in the 'pide' table. 
+ * Maps database order transactions directly to Java objects.
+ */
 public class Pedido {
-	private int idPedido,idMesa;
-	
-	public Pedido(int idPed,int idMe) {
-		this.idPedido=idPed;
-		this.idMesa=idMe;
-	}
+    private int orderId;
+    private int tableId;
+    private int productId;
+    private int amount;
+    private double totalPrice;
+    
+    /**
+     * @method Pedido
+     * @description Parameterized constructor to instantiate a complete Order model.
+     * @param {int} orderId - Unique identifier of the order transaction.
+     * @param {int} tableId - Foreign key identifier referencing the table.
+     * @param {int} productId - Foreign key identifier referencing the product.
+     * @param {int} amount - Quantity of products requested.
+     * @param {double} totalPrice - Aggregated price total for this specific entry.
+     */
+    public Pedido(int orderId, int tableId, int productId, int amount, double totalPrice) {
+        this.orderId = orderId;
+        this.tableId = tableId;
+        this.productId = productId;
+        this.amount = amount;
+        this.totalPrice = totalPrice;
+    }
 
-	public int getIdPedido() {
-		return idPedido;
-	}
+    /**
+     * @method getOrderId
+     * @returns {int} The unique identifier of the order.
+     */
+    public int getOrderId() {
+        return this.orderId;
+    }
 
-	public void setIdPedido(int idPedido) {
-		this.idPedido = idPedido;
-	}
+	/**
+	 * @method setOrderId
+	 * @param orderId
+	 */
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 
-	public int getIdMesa() {
-		return idMesa;
-	}
+    /**
+     * @method getTableId
+     * @returns {int} The table identifier linked to this order.
+     */
+    public int getTableId() {
+        return this.tableId;
+    }
 
-	public void setIdMesa(int idMesa) {
-		this.idMesa = idMesa;
-	}
-	
+	/**
+	 * @method setTableId
+	 * @param tableId
+	*/
+    public void setTableId(int tableId) {
+        this.tableId = tableId;
+    }
 
+    /**
+     * @method getProductId
+     * @returns {int} The product identifier linked to this order.
+     */
+    public int getProductId() {
+        return this.productId;
+    }
+
+    /**
+     * @method setProductId
+     * @param productId
+     */
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    /**
+     * @method getAmount
+     * @returns {int} The total units of the product.
+     */
+    public int getAmount() {
+        return this.amount;
+    }
+
+    /**
+     * @method setAmount
+     * @param amount
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @method getTotalPrice
+     * @returns {double} The financial total calculated for this entry.
+     */
+    public double getTotalPrice() {
+        return this.totalPrice;
+    }
+
+    /**
+     * @method setTotalPrice
+     * @param totalPrice
+     */
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
